5aa24a25699f0cdd46413811ca84223c5eef6cac
AlineMadruga <madruga.alinerm@gmail.com>
2021-06-20 15:23:53 -0300

    exercicio loops 1

---
+++ exercicio_loops/loops_1.js
@@ -0,0 +1,22 @@
+// Repetir como um papagaio
+
+for (i=0; i<5; i++){
+    console.log("Olá mundo!");
+}
+
+// Contando números ímpares
+
+for (i=0; i<10; i++){
+    if (i%2!=0){
+        console.log(i)
+    }
+}
+
+// Criando a tabuada
+
+for (i=1; i<=10; i++){
+    console.log("Tabuada do  " + i)
+    for (j=1; j<=10; j++){
+        console.log(i + " x " + j + " = " + i*j)
+    }
+}
